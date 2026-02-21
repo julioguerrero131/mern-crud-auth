18de27192714cf15d98a898e84fb129bcaaf2897
Julio Guerrero Alvarez <116978348+julioguerrero131@users.noreply.github.com>
2026-02-21 09:48:01 -0500

    feat: registro de usuarios desde el frontend con peticiones por axios, uso de cors en backend para permitir al frontend el registro

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import morgan from "morgan";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 import authRoutes from "./routes/auth.routes.js";
 import taskRoutes from "./routes/tasks.routes.js";
@@ -10,6 +11,10 @@ import taskRoutes from "./routes/tasks.routes.js";
 
 const app = express();
 
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+}));
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(cookieParser());
