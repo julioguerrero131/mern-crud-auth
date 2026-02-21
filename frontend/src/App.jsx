bf7908fa2f2d305882c0d7889834d09d30e866a1
Julio Guerrero Alvarez <116978348+julioguerrero131@users.noreply.github.com>
2026-02-21 08:28:25 -0500

    feat: enrutamiento iniciado con react-router-dom

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router"
+
 function App() {
   return (
-    <div className="bg-gray-900 text-white min-h-screen">App</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<h1>Home</h1>} />
+        <Route path="/login" element={<h1>Login</h1>} />
+        <Route path="/register" element={<h1>Register</h1>} />
+        <Route path="/tasks" element={<h1>All Task</h1>} />
+        <Route path="/add-task" element={<h1>Add Task</h1>} />
+        <Route path="/tasks/:id" element={<h1>One Task</h1>} />
+        <Route path="/profile" element={<h1>Profile</h1>} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
